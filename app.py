2068ad3d8b312bd816c864a62d7dc1d1cbf85193
Sebaastianrubio <as.rubiof@uea.edu.ec>
2025-08-23 01:57:49 -0500

    Agregadas plantillas dinámicas con Jinja2

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola! Bienvenido a mi aplicación utilizando flask.'
+    return render_template('index.html')
 
-@app.route('/usuario/<sebas>')
-def usuario(sebas):
-    return f'Bienvenido, {sebas}!'
-
-@app.route('/contacto')
-def contacto(sebastian):
-    return "Página de contacto: puedes escribirnos a contacto@miweb.com"
-
-@app.route('/contacto/<persona>')
-def contacto_persona(persona):
-    return f"Hola {persona}, gracias por visitar la página de contacto."
-
-@app.route('/acerca/<tema>')
-def acerca(tema):
-    return f"Sección Acerca de: aquí encontrarás información sobre {tema}."
-
-
-
+@app.route('/acerca')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
